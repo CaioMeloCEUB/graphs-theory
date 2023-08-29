73c446ae06dd76b4528b76910d724864cad9a214
Ivan Yared <ivanyared@hotmail.com>
2023-08-28 21:14:23 -0300

    Primeiro commit

---
+++ Sala_01/Main.java
@@ -0,0 +1,11 @@
+import classesJava.Node;
+
+public class Main {
+
+    public static void main (String[] args) {
+        Node n1 = new Node("A");
+        Node n2 = new Node("B");
+
+        
+    }
+}
---
+++ Sala_01/classesJava/Graph.java
@@ -0,0 +1,5 @@
+package classesJava;
+
+public class Graph {
+    
+}
