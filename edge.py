55061cafd474c9875c7fd3aa646893bb4d550d9e
Giulia <giulia.gyule@sempreceub.com>
2023-08-28 21:34:53 -0300

    teste

---
+++ edge.py
@@ -0,0 +1,9 @@
+class Edge(object):
+
+    def __init__(self, to, weight):
+
+        self.to = to
+        self.weight = weight
+
+
+
---
+++ vertex.py
@@ -0,0 +1,6 @@
+class Vertex(object):
+
+    def __init__(self, label):
+
+        self.label = label
+        self.edges = []
