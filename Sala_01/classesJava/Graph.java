446bab84c74f487596575e041eba2abd0a79a7fb
Ivan Yared <ivanyared@hotmail.com>
2023-08-28 21:25:24 -0300

    Criação da classe Graph

--- Sala_01/classesJava/Graph.java
+++ Sala_01/classesJava/Graph.java
@@ -1,5 +1,13 @@
 package classesJava;
+import classesJava.Node;
+import java.util.List;
 
 public class Graph {
+    private String name;
+    private List<Node> vertexes;
     
+    public Graph(String n, List<Node> list) {
+        this.name = n;
+        this.vertexes = list;
+    }
 }
