19c9baa6e17e54551fdac325258962612b54e5b1
Marcus Vinícius Nóbrega Santos <marcus.nobrega@dinfor.uniceub>
2023-08-28 22:14:43 -0300

    Alteração no codigo

--- Av02/Graph.java
+++ Av02/Graph.java
@@ -1,7 +1,16 @@
 package Av02;
+import java.util.*;
 import Av02.Edge;
 import Av02.Node;
 
 public class Graph {
+    List<Node> nodes;
+
+    public Graph(int numNodes) {
+        nodes = new ArrayList<>();
         
+        for (int i = 0; i < numNodes; i++) {
+            
+        }
+    }
 }
