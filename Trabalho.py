2df3e2bf922b131710419ab46f7986706492ae52
lucashls07 <111093944+lucashls07@users.noreply.github.com>
2023-08-30 09:58:24 -0300

    Add files via upload

--- Trabalho.py
+++ Trabalho.py
@@ -49,3 +49,16 @@ for i in range(num_arestas):
 # Exibir a matriz de adjacência
 print("\nMatriz de Adjacência:")
 print(meu_grafo.matriz_para_string())
+
+# Exibir o número de vértices
+num_vertices_total = len(meu_grafo.vertices)
+print(f"Número de vértices no grafo: {num_vertices_total}")
+
+# Exibir a quantidade de graus de cada vértice
+for vertice in meu_grafo.vertices:
+    grau = meu_grafo.grau_vertice(vertice)
+    print(f"Grau do vértice {vertice}: {grau}")
+
+# Calcular e exibir o grau total do grafo
+grau_total = sum(meu_grafo.grau_vertice(vertice) for vertice in meu_grafo.vertices)
+print(f"Grau total do grafo: {grau_total}")
